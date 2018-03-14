4d2a4e458a0e79379457817964176783ffea7b93
Mat Bettinson <mat@plothatching.com>
2018-03-14 20:53:21 +0930

    lifecycle done right

--- src/components/annotate/annotate.tsx
+++ src/components/annotate/annotate.tsx
@@ -1,5 +1,10 @@
 import { Component, Element, Event, Method, EventEmitter } from '@stencil/core'
-import { default as WaveSurfer } from 'wavesurfer.js'
+//import { default as WaveSurfer, TimeLinePlugin } from 'wavesurfer.js'
+import WaveSurfer from 'wavesurfer.js'
+import TimelinePlugin from 'wavesurfer.js/dist/plugin/wavesurfer.timeline.min.js'
+import MinimapPlugin from 'wavesurfer.js/dist/plugin/wavesurfer.minimap.min.js'
+import RegionsPlugin from 'wavesurfer.js/dist/plugin/wavesurfer.regions.min.js'
+
 export interface AnnotateElement extends HTMLElement, Annotate {}
 
 @Component({
@@ -19,15 +24,24 @@ export class Annotate {
   //  Class Variables
   //
   waveContainer: HTMLElement
+  timelineContainer: HTMLElement
   wavesurfer: any
   // 
   // Lifecycle
   //
   componentDidLoad() {
     this.waveContainer = this.el.shadowRoot.querySelector("#wavesurfer") 
+    this.timelineContainer = this.el.shadowRoot.querySelector("#timeline") 
     this.wavesurfer = WaveSurfer.create({
       container: this.waveContainer,
-      height: 150
+      height: 150,
+      plugins: [
+        TimelinePlugin.create({
+            container: this.timelineContainer
+        }),
+        MinimapPlugin.create(),
+        RegionsPlugin.create()
+      ]
     })
   }
 
@@ -47,6 +61,7 @@ export class Annotate {
   render() {
     return (
 <div id="root">
+  <div id="timeline"></div>
   <div id="wavesurfer"></div>
 </div>   
     )
